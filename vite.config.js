13af7cab6a8071e6c11ecf2cebf89cd86990342e
Uzan <Uzan@exemple.com>
2026-01-19 09:45:43 +0100

    Modify config vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+
+  server: {
+    port: 5173,
+    strictPort: true, // échoue si le port est déjà pris
+    open: true,       // ouvre le navigateur automatiquement
+  },
+});
